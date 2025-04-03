13b8d0c788639a2158a2d8d32febcc86a206a410
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2025-04-03 10:25:42 -0300

    enviando dados para o endpoint
    inProgress

--- Front/roboFront/src/app/robo-head/robo-head.component.ts
+++ Front/roboFront/src/app/robo-head/robo-head.component.ts
@@ -1,22 +1,46 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importando CommonModule
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-robo-head',
   standalone: true,
   templateUrl: './robo-head.component.html',
   styleUrls: ['./robo-head.component.css'],
-  imports: [CommonModule] // Adicionando CommonModule para habilitar o ngClass
+  imports: [CommonModule],
 })
 export class RoboHeadComponent {
   horizontalPosition: string = 'center';
   verticalPosition: string = 'neutral';
 
+  constructor(private http: HttpClient) {} // Injeção pelo construtor
+
   setHorizontalPosition(newPosition: string) {
     this.horizontalPosition = newPosition;
+    this.sendPositionToBackend();
   }
 
   setVerticalPosition(newPosition: string) {
     this.verticalPosition = newPosition;
+    this.sendPositionToBackend();
+  }
+
+  private sendPositionToBackend() {
+    const data = {
+      isLeftMax: this.horizontalPosition === 'left-max',
+      isLeftMid: this.horizontalPosition === 'left-mid',
+      isCenter: this.horizontalPosition === 'center',
+      isRightMid: this.horizontalPosition === 'right-mid',
+      isRightMax: this.horizontalPosition === 'right-max',
+      isUp: this.verticalPosition === 'up',
+      isNeutral: this.verticalPosition === 'neutral',
+      isDown: this.verticalPosition === 'down',
+    };
+    console.log('Dados a serem enviados:', data); // Log dos dados a serem enviados
+
+    this.http.post('https://seu-endpoint.com/posicao-robo', data).subscribe({
+      next: response => console.log('Posição enviada:', response),
+      error: error => console.error('Erro ao enviar posição:', error),
+    });
   }
 }
